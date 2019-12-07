ed18e7072acc12e8b1307ac5ecce10305769e3f8
mike <mike@bfo.com>
2019-12-07 16:24:59 +0000

    * Added MQTT publishing with libmosquitto * Fixed loss of sync due to coming into a frame halfway through

--- src/cm160.c
+++ src/cm160.c
@@ -25,6 +25,8 @@
 #include <usb.h>
 #include <pthread.h>
 #include <fcntl.h>
+#include <time.h>
+#include <mosquitto.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -56,7 +58,21 @@ static char WAIT_MSG[11] = {
 
 struct cm160_device g_devices[MAX_DEVICES];
 static unsigned char history[HISTORY_SIZE][11];
+static char *mqtt_host;
+static char *mqtt_topic = "cm160";
+static int mqtt_port = 1883;
+static time_t mqtt_reconnect = 0;
+static struct mosquitto *mqtt = NULL;
 
+// Mosquitto callbacks
+void connect_callback(struct mosquitto *mqtt, void *obj, int result) {
+  printf("connect callback, rc=%d\n", result);
+  if (result == 0) {
+    mqtt_reconnect = 0;
+  }
+}
+
+// Data processing
 static void process_live_data(struct record_data *rec) 
 {
   static double _watts = -1;
@@ -75,17 +91,34 @@ static void process_live_data(struct record_data *rec)
   if(fp)
   {
     if(rec->hour!=255) // to avoid writing strange values (i.e. date 2255, hour 255:255) that sometimes I got
-      fprintf(fp, "%02d/%02d/%04d %02d:%02d - %.02f kW\n", 
-              rec->day, rec->month, rec->year, rec->hour, rec->min, w);
+      fprintf(fp, "%02d/%02d/%04d %02d:%02d - %.02f kW\n", rec->day, rec->month, rec->year, rec->hour, rec->min, w);
     fclose(fp);
   }
+}
+
+static void publish(struct record_data *rec) {
+  static char buf[80];
+  static struct tm tm;
+  memset(&tm, 0, sizeof(struct tm));
+//  tm.tm_sec = rec->sec;
+  tm.tm_min = rec->min;
+  tm.tm_hour = rec->hour;
+  tm.tm_mday = rec->day;
+  tm.tm_mon = rec->month - 1;
+  tm.tm_year = rec->year - 1900;
+  time_t t = mktime(&tm);       // local time
+  if (rec->isLiveData) {
+      t = time(NULL);   // why would I care about time on device?
+  }
+  snprintf(buf, sizeof(buf), "{\"type\":\"%s\",\"watts\":%1f,\"when\":%ld}", rec->isLiveData ? "live": "hist", rec->watts, t);
+  mosquitto_publish(mqtt, NULL, mqtt_topic, strlen(buf), buf, 0, 0);
 }
 
 static void decode_frame(unsigned char *frame, struct record_data *rec)
 {
   int volt = 230; // TODO: use the value from energy_param table (supply_voltage)
   rec->addr = 0; // TODO: don't use an harcoded addr value for the device...
-  rec->year = frame[1]+2000;
+  rec->year = frame[1] + 2000;
   rec->month = frame[2];
   rec->day = frame[3];
   rec->hour = frame[4];
@@ -124,19 +157,18 @@ void insert_db_history(void *data)
 
     db_insert_hist(&rec);
     printf("\r %.1f%%", min(100, 100*((double)i/num_elems)));
-    fflush(stdout);
   }
   db_update_status();
   db_end_transaction();
 
   printf("\rinsert into db... 100%%\n");
-  fflush(stdout);
   printf("update db in %4.2f seconds\n", 
          (clock() - cStartClock) / (double)CLOCKS_PER_SEC);
 }
 
 bool receive_history = true;
 int frame_id = 0;
+
 static int process_frame(int dev_id, unsigned char *frame)
 {
   int i;
@@ -145,16 +177,20 @@ static int process_frame(int dev_id, unsigned char *frame)
   static int last_valid_month = 0; 
   usb_dev_handle *hdev = g_devices[dev_id].hdev;
   int epout = g_devices[dev_id].epout;
+//  for (i=0;i<11;i++) {
+//    printf("%02x ", frame[i]);
+//  }
+//  printf("\n");
 
   if(strncmp((char *)frame, ID_MSG, 11) == 0)
   {
-//    printf("received ID MSG\n");
+    printf("received ID MSG\n");
     data[0]=0x5A;
     usb_bulk_write(hdev, epout, (const char *)&data, sizeof(data), 1000);
   }
   else if(strncmp((char *)frame, WAIT_MSG, 11) == 0)
   {
-//    printf("received WAIT MSG\n");
+    printf("received WAIT MSG\n");
     data[0]=0xA5;
     usb_bulk_write(hdev, epout, (const char *)&data, sizeof(data), 1000);
   }
@@ -162,43 +198,41 @@ static int process_frame(int dev_id, unsigned char *frame)
   {
     if(frame[0] != FRAME_ID_LIVE && frame[0] != FRAME_ID_DB)
     {
-      printf("data error: invalid ID 0x%x\n", frame[0]);
-      for(i=0; i<11; i++)
-        printf("0x%02x - ", frame[i]);
-      printf("\n");
+//      printf("data error: invalid ID 0x%x\n", frame[0]);
       return -1;
     }
 
-    for(i=0; i<10; i++)
+    for(i=0; i<10; i++) {
       checksum += frame[i];
+    }
     checksum &= 0xff;
     if(checksum != frame[10])
     {
-      printf("data error: invalid checksum: expected 0x%x, got 0x%x\n", 
-             frame[10], checksum);
+//      printf("data error: invalid checksum: expected 0x%x, got 0x%x\n", frame[10], checksum);
       return -1;
     }
 
     struct record_data rec;
     decode_frame(frame, &rec);
 
-    if(rec.month < 0 || rec.month > 12)
+    if(rec.month < 0 || rec.month > 12) {
       rec.month = last_valid_month;
-    else
+    } else {
       last_valid_month = rec.month;
+    }
 
     if(frame[0]==FRAME_ID_DB)
     {
       if(receive_history && frame_id < HISTORY_SIZE)
       {
-        if(frame_id == 0)
+        if(frame_id == 0) {
           printf("downloading history...\n");
+        }
         else if(frame_id%10 == 0)
         { // print progression status
           // rough estimation : we should received a month of history
           // -> 31x24x60 minute records
           printf("\r %.1f%%", min(100, 100*((double)frame_id/(31*24*60))));
-          fflush(stdout);
         }
         // cache the history in a buffer, we will insert it in the db later.
         memcpy(history[frame_id++], frame, 11);
@@ -218,7 +252,6 @@ static int process_frame(int dev_id, unsigned char *frame)
       { // When we receive the first live data, 
         // we know that the history is totally downloaded
         printf("\rdownloading history... 100%%\n");
-        fflush(stdout);
         receive_history = false;
         // Now, insert the history into the db
         pthread_t thread;
@@ -228,6 +261,7 @@ static int process_frame(int dev_id, unsigned char *frame)
       process_live_data(&rec);
       printf("LIVE: %02d/%02d/%04d %02d:%02d : %f W\n",
              rec.day, rec.month, rec.year, rec.hour, rec.min, rec.watts);
+      publish(&rec);
     }
   }
   return 0;
@@ -240,27 +274,50 @@ static int io_loop(int dev_id)
   int epin = g_devices[dev_id].epin;
   unsigned char buffer[512];
   unsigned char word[11];
+  int j = -1;
 
   memset(buffer, 0, sizeof(buffer));
   memset(word, 0, sizeof(word));
 
-  while(1)
-  {
+  while(1) {
     memset(buffer, 0, sizeof(buffer));
     ret = usb_bulk_read(hdev, epin, (char*)buffer, sizeof(buffer), 10000);
-    if(ret < 0)
-    {
+    if(ret < 0) {
       printf("bulk_read returned %d (%s)\n", ret, usb_strerror());
       return -1;
     }
-//    printf("read %d bytes: \n", ret);
-    unsigned char *bufptr = (unsigned char *)buffer;
-    int nb_words = ret/11; // incomplete words are resent
-    while(nb_words--)
-    {
-      memcpy(word, bufptr, 11);
-      bufptr+=11;
-      process_frame(dev_id, word);
+    // Cycle through read bytes trying to find a complete frame.
+    // This approach will automatically restore sync if we come in
+    // halfway through a frame, which happens.
+    for (int i=0;i<ret;i++) {
+      unsigned char c = buffer[i];
+      if (j < 10) {
+        word[j++] = c;
+      } else if (j == 10) {
+        word[j++] = c;
+        if (!process_frame(dev_id, word)) {
+          j = 0;
+        }
+      } else {
+        for (int k=0;k<10;k++) {
+          word[k] = word[k + 1];
+        }
+        word[10] = c;
+        if (!process_frame(dev_id, word)) {
+          j = 0;
+        }
+      }
+    }
+
+    if (mqtt_reconnect == 0) {
+      int rc = mosquitto_loop(mqtt, -1, 1);
+      if (rc) {
+        printf("Lost MQTT connection to \"%s\", retrying in 10s", mqtt_host);
+        mqtt_reconnect = time(NULL) + 10;
+      }
+    } else if (time(NULL) > mqtt_reconnect) {
+      mqtt_reconnect = time(NULL) + 10;
+      mosquitto_reconnect(mqtt);
     }
   }
   return 0;
@@ -313,11 +370,45 @@ static int handle_device(int dev_id)
 }
 
 
+void usage(char *host) {
+  fprintf(stderr, "Usage: %s [-d] [--mqtt-host <host>] [--mqtt-port <port>] [--mqtt-client <name>]\n", host);
+  exit(1);
+}
+
 int main(int argc, char **argv)
 {
   int dev_cnt;
-  if(argc>1 && (strcmp(argv[1], "-d")==0) )
+  bool daemonize = false;
+  char *mqtt_clientid = "cm160";
+
+  for (int i=1;i<argc;i++) {
+    if (!strcmp(argv[i], "-d")) {
+      daemonize = true;
+    } else if (!strcmp(argv[i], "--mqtt-host")) {
+      mqtt_host = argv[++i];
+    } else if (!strcmp(argv[i], "--mqtt-port")) {
+      mqtt_port = atoi(argv[++i]);
+    } else if (!strcmp(argv[i], "--mqtt-client")) {
+      mqtt_clientid = argv[++i];
+    } else if (!strcmp(argv[i], "--mqtt-topic")) {
+      mqtt_topic = argv[++i];
+    } else {
+      usage(argv[0]);
+    }
+  }
+
+  if (daemonize) {
     demonize(argv[0]);
+  }
+  if (mqtt_host != NULL) {
+    mqtt = mosquitto_new(mqtt_clientid, true, 0);
+    if (!mqtt) {
+      perror("Unable to allocate");
+      exit(1);
+    }
+    mosquitto_connect_callback_set(mqtt, connect_callback);
+    mosquitto_connect(mqtt, mqtt_host, mqtt_port, 60);
+  }
 
   while(1)
   {
@@ -326,8 +417,9 @@ int main(int argc, char **argv)
     receive_history = true;
     frame_id = 0;
     printf("Wait for cm160 device to be connected\n");
-    while((dev_cnt = scan_usb()) == 0)
+    while((dev_cnt = scan_usb()) == 0) {
       sleep(2);
+    }
     printf("Found %d compatible device%s\n", dev_cnt, dev_cnt>1?"s":"");
 
     // Only 1 device supported
